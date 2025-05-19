375e2b76594b0d5f12fa1d324eef78aedba9d800
RandoSky <ovinkin.kir@gmail.com>
2025-05-20 01:46:30 +0500

    Поправил authAxiosInstance

--- frontend/src/api/authInstance.ts
+++ frontend/src/api/authInstance.ts
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
-import axios, { type InternalAxiosRequestConfig } from "axios";
+import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
+
+import { FORBIDDEN_CODE } from "@/config";
 
 const baseURL = import.meta.env.VITE_API_URL;
 
@@ -18,17 +20,36 @@ const authInterceptor = (config: InternalAxiosRequestConfig): InternalAxiosReque
 };
 
 /** Устанавливаем accessToken в sessionStorage после запроса */
-authAxiosInstance.interceptors.response.use(response => {
-  const {
-    data: { accessToken },
-  } = response;
+authAxiosInstance.interceptors.response.use(
+  response => {
+    const {
+      data: { accessToken },
+    } = response;
 
-  if (typeof accessToken === "string") {
-    sessionStorage.setItem("accessToken", accessToken);
+    if (typeof accessToken === "string") {
+      sessionStorage.setItem("accessToken", accessToken);
+    }
+
+    return response;
+  },
+  async (error: AxiosError) => {
+    const { response } = error;
+
+    if (
+      response?.status === FORBIDDEN_CODE ||
+      (response?.data as { message?: string })?.message === "Токен отсутствует"
+    ) {
+      sessionStorage.removeItem("user");
+      sessionStorage.removeItem("accessToken");
+      window.location.reload();
+
+      return;
+    }
+
+    // Пробрасываем оригинальную ошибку
+    return Promise.reject(error);
   }
-
-  return response;
-});
+);
 
 authAxiosInstance.interceptors.request.use(authInterceptor);
 
